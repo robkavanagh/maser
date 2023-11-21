03bb80bea90ffad238bc2012be77dda4d7ed400e
Benjamin Pope <b.pope@uq.edu.au>
2023-11-21 20:53:31 +1000

    rebuild to work with Jax instead of numpy

--- src/maser/__init__.py
+++ src/maser/__init__.py
@@ -1,4 +1,4 @@
 name = "maser"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 from.maser import *
--- src/maser/maser.py
+++ src/maser/maser.py
@@ -5,6 +5,14 @@
 # Version 1.0
 
 import numpy as np
+import jax.numpy as jnp
+
+# from jax import config 
+# config.update("jax_enable_x64", True) #  64-bit precision to match Rob's code in accuracy - turns out this is not important
+
+from jax import jit, vmap
+
+step = lambda x: jnp.heaviside(x, 1.0)
 
 # Constants
 Msun = 1.989e33
@@ -41,22 +49,135 @@ zero_vec = np.array([0.0, 0.0, 0.0])
 # 
 # The function returns two arrays of boolean values, which infer if emission is visible from the Northern (vis_N) and Southern (vis_S) magnetic hemispheres at each element of the times array.
 
+
+def get_rf(B_f,B_s,L):
+
+	# Initialise solver
+	r_i = 1
+
+	for j in range(12):
+		B_i = B_s * (1 - 0.75 / L) ** 0.5
+
+	# Loop until frequency difference is less than tolerance set
+
+		F_i = (B_f / B_s) ** 2 * r_i ** 6. + 0.75 * r_i / L - 1
+		dF_i = 6 * (B_f / B_s) ** 2 * r_i ** 5. + 0.75 / L
+		r_i = r_i - F_i / dF_i
+		B_i = B_s * r_i ** -3. * (1 - 0.75 * r_i / L) ** 0.5
+
+	converged =  jnp.abs(1 - B_i / B_f)
+
+	return r_i, converged
+
+def vis_t(t, P_s, B_s, beta, phi_s0, a, i_p, phi_p0, f, alpha, dalpha,
+								x, y, n_s, z_s, y1, z1,P_p):
+	##################################################
+	# Compute vectors
+	##################################################
+
+	# Rotation phase (rad)
+	phi_s = 2 * jnp.pi * (phi_s0 + t / P_s)
+
+	# Planet orbital phase (rad)
+	phi_p = 2 * jnp.pi * (phi_p0	+ t / P_p)
+
+	# Stellar coordinates
+	x_s = jnp.cos(phi_s) * n_s + jnp.sin(phi_s) * y
+
+	# Magnetic pole
+	z_B = jnp.sin(beta) * x_s + jnp.cos(beta) * z_s
+
+	# Planet coordinates
+	n_p = jnp.sin(i_p) * x - jnp.cos(i_p) * z1
+	x_p = jnp.sin(phi_p) * y1 + jnp.cos(phi_p) * n_p
+
+	# Magnetic latitude of planet
+	cos_theta_p = jnp.sum(x_p * z_B)
+	sin_theta_p = (1. - cos_theta_p ** 2) ** 0.5
+
+	# Loop size
+	L = a / (sin_theta_p) ** 2 # fudge the infinity
+
+	# Min and max frequency on field line
+	f_min = 1.4 * B_s * L ** -3.
+	f_max = 2.8 * B_s * (1. - 0.75 / L) ** 0.5
+
+	# Planet frequency (MHz)
+	f_p = 2.8 * B_s * (a ** -3.) * (1 - 0.75 * a / L) ** 0.5
+
+	# Skip time if emission cannot occur at frequency f on the field line
+	can_occur_on_line = jnp.max(jnp.array([step(f-f_min) * step(f_max-f), step(Lmax-L) * step(f-f_p)]))
+	# if ((f > f_max) | (f < f_min)) | ((L > Lmax) & (f_p > f)): 
+	# 	return 0., 0.
+
+	# Magnetic equator
+	# if L == jnp.inf: x_B = zero_vec # this never happens
+	x_B = x_p / sin_theta_p - (cos_theta_p / sin_theta_p) * z_B
+
+	##################################################
+	# Solve for radius of emitting point
+	##################################################
+
+	# Field strength of emitting point
+	B_f = f / 2.8
+
+	r_f, converged = get_rf(B_f,B_s,L)
+
+	##################################################
+	# Visibility
+	##################################################
+
+	# Compute latitude of emission point in Northern hemisphere
+	sin_theta_f = (r_f / L) ** 0.5
+	cos_theta_f = (1 - sin_theta_f ** 2) ** 0.5
+
+	# Cone components in Northern hemisphere in terms of x_B and z_B
+	c_x = 3. * sin_theta_f * cos_theta_f / (1. + 3. * cos_theta_f ** 2) ** 0.5
+	c_z = (3. * cos_theta_f ** 2. - 1.) / (1. + 3. * cos_theta_f ** 2) ** 0.5
+
+	# Check if emission generated in Northern hemisphere
+	occurs_in_north = jnp.max(jnp.array([step(f-f_p)*step(cos_theta_p), step(-cos_theta_p)*step(Lmax-L)]))
+
+	# Cone axis
+	c_N = c_x * x_B + c_z * z_B
+
+	# Angle between cone and line of sight (rad)
+	gamma_N = jnp.arccos(c_N[0])
+
+	# Visibility
+	occurs_in_north *= step(gamma_N - (alpha - dalpha / 2)) * step((alpha + dalpha / 2) - gamma_N)	
+
+	vis_N = can_occur_on_line*occurs_in_north
+
+	# Check if emission generated in Southern hemisphere
+	occurs_in_south = jnp.max(jnp.array([step(f-f_p)*step(-cos_theta_p), step(cos_theta_p)*step(Lmax-L)]))
+
+	# Cone axis
+	c_S = c_x * x_B - c_z * z_B
+
+	# Angle between cone and line of sight (rad)
+	gamma_S = jnp.arccos(c_S[0])
+
+		# Visibility
+	occurs_in_south *= step(gamma_S - (alpha - dalpha / 2)) * step((alpha + dalpha / 2) - gamma_S)
+
+	vis_S = can_occur_on_line*occurs_in_south
+
+	return vis_N, vis_S#, converged # for diagnostics
+
+# Line of sight axes
+x = jnp.array([1, 0, 0])
+y = jnp.array([0, 1, 0])
+z = jnp.array([0, 0, 1])
+
+@jit
 def maser(M_s, R_s, P_s, i_s, B_s, beta, phi_s0, a, i_p, lam, phi_p0, f, alpha, dalpha, times):
 
-	# Line of sight axes
-	x = np.array([1, 0, 0])
-	y = np.array([0, 1, 0])
-	z = np.array([0, 0, 1])
-
-	# Visibility arrays
-	ntimes = len(times)
-	vis = np.full((2, ntimes), False)
-
 	# Skip sample if emission at frequency f is not possible
-	if f > 2.8 * B_s: return vis
+	frequency_possible = step(2.8*B_s-f)
 
 	# Planet orbital period (days)
-	P_p = 2 * np.pi * ((a * R_s * Rsun) ** 3 / (G * M_s * Msun)) ** 0.5 / sec_per_day
+	P_p = 2 * jnp.pi * ((a * R_s * Rsun) ** 3 / (G * M_s * Msun)) ** 0.5 / sec_per_day
 
 	# Angles to rad
 	i_s *= rad_per_deg
@@ -67,10 +188,10 @@ def maser(M_s, R_s, P_s, i_s, B_s, beta, phi_s0, a, i_p, lam, phi_p0, f, alpha, 
 	dalpha *= rad_per_deg
 
 	# Trig terms
-	sin_i_s = np.sin(i_s)
-	cos_i_s = np.cos(i_s)
-	sin_lam = np.sin(lam)
-	cos_lam = np.cos(lam)
+	sin_i_s = jnp.sin(i_s)
+	cos_i_s = jnp.cos(i_s)
+	sin_lam = jnp.sin(lam)
+	cos_lam = jnp.cos(lam)
 
 	# Stellar coordinates
 	z_s = cos_i_s * x + sin_i_s * z
@@ -85,107 +206,7 @@ def maser(M_s, R_s, P_s, i_s, B_s, beta, phi_s0, a, i_p, lam, phi_p0, f, alpha, 
 	# Time loop
 	##################################################
 
-	for j, t in enumerate(times):
+	vis_N, vis_S = vmap(lambda t: vis_t(t, P_s, B_s, beta, phi_s0, a, i_p, phi_p0, f, alpha, dalpha,
+								x, y, n_s, z_s, y1, z1,P_p))(times)
 
-		##################################################
-		# Compute vectors
-		##################################################
-
-		# Rotation phase (rad)
-		phi_s = 2 * np.pi * (phi_s0 + t / P_s)
-
-		# Planet orbital phase (rad)
-		phi_p = 2 * np.pi * (phi_p0	+ t / P_p)
-
-		# Stellar coordinates
-		x_s = np.cos(phi_s) * n_s + np.sin(phi_s) * y
-
-		# Magnetic pole
-		z_B = np.sin(beta) * x_s + np.cos(beta) * z_s
-
-		# Planet coordinates
-		n_p = np.sin(i_p) * x - np.cos(i_p) * z1
-		x_p = np.sin(phi_p) * y1 + np.cos(phi_p) * n_p
-
-		# Magnetic latitude of planet
-		cos_theta_p = np.sum(x_p * z_B)
-		sin_theta_p = (1 - cos_theta_p ** 2) ** 0.5
-
-		# Loop size
-		if sin_theta_p == 0: L = np.inf
-		else: L = a / sin_theta_p ** 2
-
-		# Min and max frequency on field line
-		f_min = 1.4 * B_s * L ** -3
-		f_max = 2.8 * B_s * (1 - 0.75 / L) ** 0.5
-
-		# Planet frequency (MHz)
-		f_p = 2.8 * B_s * (a ** -3) * (1 - 0.75 * a / L) ** 0.5
-
-		# Skip time if emission cannot occur at frequency f on the field line
-		if ((f > f_max) | (f < f_min)) | ((L > Lmax) & (f_p > f)): continue
-
-		# Magnetic equator
-		if L == np.inf: x_B = zero_vec
-		else: x_B = x_p / sin_theta_p - (cos_theta_p / sin_theta_p) * z_B
-
-
-		##################################################
-		# Solve for radius of emitting point
-		##################################################
-
-		# Field strength of emitting point
-		B_f = f / 2.8
-
-		# Initialise solver
-		r_i = 1
-		B_i = B_s * (1 - 0.75 / L) ** 0.5
-
-		# Loop until frequency difference is less than tolerance set
-		while np.abs(1 - B_i / B_f) > tol:
-
-			F_i = (B_f / B_s) ** 2 * r_i ** 6 + 0.75 * r_i / L - 1
-			dF_i = 6 * (B_f / B_s) ** 2 * r_i ** 5 + 0.75 / L
-			r_i = r_i - F_i / dF_i
-			B_i = B_s * r_i ** -3 * (1 - 0.75 * r_i / L) ** 0.5
-
-		r_f = r_i
-
-
-		##################################################
-		# Visibility
-		##################################################
-
-		# Compute latitude of emission point in Northern hemisphere
-		sin_theta_f = (r_f / L) ** 0.5
-		cos_theta_f = (1 - sin_theta_f ** 2) ** 0.5
-
-		# Cone components in Northern hemisphere in terms of x_B and z_B
-		c_x = 3 * sin_theta_f * cos_theta_f / (1 + 3 * cos_theta_f ** 2) ** 0.5
-		c_z = (3 * cos_theta_f ** 2 - 1) / (1 + 3 * cos_theta_f ** 2) ** 0.5
-
-		# Check if emission generated in Northern hemisphere
-		if ((cos_theta_p >= 0) & (f > f_p)) | ((cos_theta_p < 0) & (L < Lmax)):
-
-			# Cone axis
-			c_N = c_x * x_B + c_z * z_B
-
-			# Angle between cone and line of sight (rad)
-			gamma_N = np.arccos(c_N[0])
-
-			# Visibility
-			vis[0, j] = (alpha - dalpha / 2 < gamma_N) & (gamma_N < alpha + dalpha / 2)
-
-		# Check if emission generated in Southern hemisphere
-		if ((cos_theta_p < 0) & (f > f_p)) | ((cos_theta_p >= 0) & (L < Lmax)):
-
-			# Cone axis
-			c_S = c_x * x_B - c_z * z_B
-
-			# Angle between cone and line of sight (rad)
-			gamma_S = np.arccos(c_S[0])
-
-			# Visibility
-			vis[1, j] = (alpha - dalpha / 2 < gamma_S) & (gamma_S < alpha + dalpha / 2)
-
-	return vis*1.0
+	return frequency_possible*jnp.array([vis_N, vis_S])
